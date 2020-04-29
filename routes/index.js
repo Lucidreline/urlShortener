404ae4abacdb898c26105071d65937c9c889c5d9
Manny <ma52castaneda@gmail.com>
2020-04-29 02:22:07 -0700

    when a user uses the shortened link, they will be redirected to the original link

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const router = express.Router();
 
+const Url = require('../models/Url')
+
+// @ route  GET /api/url/code
+// @ desc  redirects to the desired url
+router.get('/:code', async (req, res) => {
+    try{
+        const url = await Url.findOne({ urlCode: req.params.code })
+        if (url){
+            return res.redirect(url.longUrl)
+        }else{
+            return res.status(401).json('No url found')
+        }
+    }catch(err){
+        console.error(err)
+        res.status(500).json('Server Error');
+    }
+})
 
 module.exports = router
