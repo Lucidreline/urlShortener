b06d084d891358f5cc09caf44a3e84e1fb57b8eb
Lucidreline <casta.ma502@gmail.com>
2021-03-23 17:11:22 -0700

    Removed the valid url checker because it was not working.

--- routes/url.js
+++ routes/url.js
@@ -9,7 +9,8 @@ const Url = require('../models/Url')
 
 // @route  POST /api/url/shorten
 // @desc   Create Short Url
-router.post('/shorten', async (req, res) => {
+router.post('/shorten', async (req, res) =>
+{
     const { longUrl } = req.body;
     const baseUrl = config.get('baseUrl')
 
@@ -20,47 +21,43 @@ router.post('/shorten', async (req, res) => {
 
     let unique = false
     let urlCode = ''
-    while(!unique){ 
+    while (!unique) {
         // create url code
         urlCode = shortId.generate()
         foundUrl = await Url.findOne({ urlCode })
 
-        if(!foundUrl){
+        if (!foundUrl) {
             unique = true
         }
     }
 
-    
+
 
     // check long url
-    if (validUrl.isUri(longUrl)) {
-        try {
-            let url = await Url.findOne({ longUrl });
+    try {
+        let url = await Url.findOne({ longUrl });
 
-            if (url) { // prevents 2 codes being given out for one same website
-                return res.render('./index', {shortUrl: url.shortUrl})
-            } else {
-                const shortUrl = baseUrl + '/' + urlCode
+        if (url) { // prevents 2 codes being given out for one same website
+            return res.render('./index', { shortUrl: url.shortUrl })
+        } else {
+            const shortUrl = baseUrl + '/' + urlCode
 
-                url = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                });
+            url = new Url({
+                longUrl,
+                shortUrl,
+                urlCode,
+                date: new Date()
+            });
 
-                await url.save()
-                
-                return res.render('./index', {shortUrl: url.shortUrl})
-                
+            await url.save()
 
-            }
-        } catch (err) {
-            console.err(err)
-            res.status(500).json('Server Error')
+            return res.render('./index', { shortUrl: url.shortUrl })
+
+
         }
-    } else {
-        res.status(401).json('Invalid Long Url')
+    } catch (err) {
+        console.err(err)
+        res.status(500).json('Server Error')
     }
 })
 
